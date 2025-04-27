f73e4fbbde153c85cbfd3baabc5cfc5c33198091
Przemysław Pietkun <116354099+Silentmaster86@users.noreply.github.com>
2025-04-27 18:15:23 +0100

    Update Sidebar.js

--- src/components/Layout/Sidebar.js
+++ src/components/Layout/Sidebar.js
@@ -3,16 +3,27 @@ import styled from "styled-components";
 import SubredditList from "../../features/posts/SubredditList.js";
 
 const SidebarContainer = styled.aside`
-  width: 350px;
-  padding: 1rem;
+  width: 300px;
+  min-width: 250px;
   background-color: #f6f7f8;
   border-right: 1px solid #ddd;
   min-height: 100vh;
+  padding: 1.5rem 1rem;
+  display: flex;
+  flex-direction: column;
+`;
+
+const SidebarHeader = styled.h2`
+  font-size: 1.4rem;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 1.5rem;
 `;
 
 const Sidebar = () => {
   return (
     <SidebarContainer>
+      <SidebarHeader>Subreddits</SidebarHeader>
       <SubredditList />
     </SidebarContainer>
   );
