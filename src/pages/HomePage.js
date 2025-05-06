e4687c8848205e61d43bc659b9f32f6d1c62e313
Przemysław Pietkun <116354099+Silentmaster86@users.noreply.github.com>
2025-05-06 22:34:48 +0100

    Update HomePage.js

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,54 +1,17 @@
 // src/pages/HomePage.js
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
 import styled from "styled-components";
-import { fetchPosts } from "../features/posts/postsSlice.js";
-import { Link } from "react-router-dom";
-import Spinner from "../components/UI/Spinner.js";
+import PostList from "../features/posts/PostList";
 
 const Wrapper = styled.div`
   padding: 2rem;
-  color:rgb(151, 155, 155);
-`;
-
-const PostCard = styled.div`
-  background: #1a1a1b;
-  border: 1px solid #343536;
-  padding: 1rem;
-  margin-bottom: 1rem;
-  border-radius: 8px;
-  transition: background 0.2s;
-  &:hover {
-    background: #2a2a2b;
-  }
+  color: rgb(151, 155, 155);
 `;
 
 const HomePage = () => {
-  const dispatch = useDispatch();
-  const { posts, status } = useSelector((state) => state.posts);
-
-  useEffect(() => {
-    dispatch(fetchPosts());
-  }, [dispatch]);
-
   return (
     <Wrapper>
-      <h1>Welcome to Reddit Clone! 🎉</h1>
-      <p>Explore trending posts. Sign in to vote, comment, or post your own.</p>
-
-      {status === "loading" && <Spinner />}
-      {status === "succeeded" && posts.length > 0 && (
-        <div>
-          {posts.slice(0, 5).map((post) => (
-            <PostCard key={post.id}>
-              <h3>{post.title}</h3>
-              <p>by u/{post.author}</p>
-              <Link to={`/post/${post.id}`}>Read more</Link>
-            </PostCard>
-          ))}
-        </div>
-      )}
-      {status === "failed" && <p>Failed to load posts. Please try again.</p>}
+      <PostList />
     </Wrapper>
   );
 };
