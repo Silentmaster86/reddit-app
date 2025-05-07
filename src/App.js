331922d515ac26913d95666fc37a06650c728150
Przemysław Pietkun <116354099+Silentmaster86@users.noreply.github.com>
2025-05-07 21:38:21 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import AuthObserver from "./features/auth/AuthObserver.js";
+import { useDispatch } from "react-redux";
+import { fetchPosts } from "./features/posts/postsSlice.js";
+import AuthObserver from "./features/auth/Authobserver.js";
 import AppRoutes from "./routes/AppRoutes.js";
+import Navbar from "./components/Layout/Navbar.js";
+import SoundBar from "./components/Layout/SoundBar.js";
+import "./styles/App.css";
+import styled from "styled-components";
+
+
+const Layout = styled.div`
+  display: flex;
+`;
+
+const Content = styled.main`
+  flex: 1;
+  padding: 2rem;
+`;
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [dispatch]);
+
   return (
     <Router>
       <AuthObserver />
-      <AppRoutes />
+      <Navbar />
+      <SoundBar />
+      <AppRoutes />    
     </Router>
   );
 }
