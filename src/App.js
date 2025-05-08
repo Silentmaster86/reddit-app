3b3809761203c7a56218fc5a7b3d403aa39a54d6
Przemysław Pietkun <116354099+Silentmaster86@users.noreply.github.com>
2025-05-08 21:35:44 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
+// src/App.js
 import React, { useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { ThemeProvider as StyledThemeProvider } from "styled-components";
+import { ThemeProvider, useTheme } from "./context/ThemeContext.js";
 import { fetchPosts } from "./features/posts/postsSlice.js";
 import AuthObserver from "./features/auth/Authobserver.js";
 import AppRoutes from "./routes/AppRoutes.js";
 import Navbar from "./components/Layout/Navbar.js";
 import SoundBar from "./components/Layout/SoundBar.js";
 import "./styles/App.css";
-import styled from "styled-components";
 
-
-const Layout = styled.div`
-  display: flex;
-`;
-
-const Content = styled.main`
-  flex: 1;
-  padding: 2rem;
-`;
-
-function App() {
+const AppContent = () => {
   const dispatch = useDispatch();
+  const { theme } = useTheme(); // from ThemeContext
 
   useEffect(() => {
     dispatch(fetchPosts());
   }, [dispatch]);
 
   return (
-    <Router>
-      <AuthObserver />
-      <AppRoutes />
-    </Router>
+    <StyledThemeProvider theme={theme}>
+      <Router>
+        <AuthObserver />
+        <Navbar />
+        <SoundBar />
+        <AppRoutes />
+      </Router>
+    </StyledThemeProvider>
+  );
+};
+
+export default function App() {
+  return (
+    <ThemeProvider>
+      <AppContent />
+    </ThemeProvider>
   );
 }
-
-
-export default App;
