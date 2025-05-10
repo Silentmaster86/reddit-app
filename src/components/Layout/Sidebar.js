6531fe4ef851c44ef56dd2bc9375dc2ea649372a
Przemysław Pietkun <116354099+Silentmaster86@users.noreply.github.com>
2025-05-11 00:37:11 +0100

    Update Sidebar.js

--- src/components/Layout/Sidebar.js
+++ src/components/Layout/Sidebar.js
@@ -2,20 +2,9 @@ import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 import SubredditList from "../../features/posts/SubredditList.js";
 
-const SidebarContainer = styled.aside`
-  width: 250px;
-  background-color: #f6f7f8;
-  border-right: 1px solid #ddd;
-  min-height: 100vh;
-  padding: 1.5rem 1rem;
-  display: flex;
-  flex-direction: column;
-
-  @media (max-width: 768px) {
-    width: 100%;
-    border-right: none;
-    padding: 1rem;
-  }
+const SidebarContainer = styled.div`
+  height: 100%;
+  overflow-y: auto;
 `;
 
 const SidebarHeader = styled.h2`
@@ -33,7 +22,7 @@ const Sidebar = ({ onLinkClick, open }) => {
       sidebarRef.current.scrollTo({ top: 0, behavior: "smooth" });
     }
   }, [open]);
-  
+
   return (
     <SidebarContainer ref={sidebarRef}>
       <SidebarHeader>Subreddits</SidebarHeader>
