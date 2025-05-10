397d9c37b8afeb9abad0a876362ddb5aac00c4f2
Przemysław Pietkun <116354099+Silentmaster86@users.noreply.github.com>
2025-05-10 12:50:42 +0100

    Update AudioContext.js

--- src/context/AudioContext.js
+++ src/context/AudioContext.js
@@ -22,6 +22,38 @@ const stations = [
     url: "https://media-ice.musicradio.com/RadioX90s",
     logo: "https://radiotoday.co.uk/wp-content/uploads/2025/02/radiox90s.jpg",
   },
+{
+  name: "RMF FM",
+  url: "https://195.150.20.244/rmf_fm",
+  logo: "https://i.ibb.co/PWRKNLz/rmf-logo.png",
+},
+{
+  name: "Radio ZET",
+  url: "https://zet090-01.cdn.eurozet.pl:8443/zet.aac",
+  logo: "https://upload.wikimedia.org/wikipedia/commons/0/06/Radio_ZET_logo.svg",
+},
+{
+  name: "Polskie Radio Trójka",
+  url: "https://stream3.polskieradio.pl/program3",
+  logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Logo_Tr%C3%B3jka_Polskie_Radio.svg/512px-Logo_Tr%C3%B3jka_Polskie_Radio.svg.png",
+},
+{
+  name: "Radio Eska",
+  url: "https://stream3.eskago.pl/1_eska_mp3",
+  logo: "https://i.ibb.co/hKXc5cD/eska.png",
+},
+{
+  name: "Radio 357",
+  url: "https://stream.rcs.pl/357",
+  logo: "https://radio357.pl/favicon/android-chrome-192x192.png",
+},
+{
+  name: "Radio Nowy Świat",
+  url: "https://stream.rcs.pl/rns",
+  logo: "https://radionowyswiat.pl/assets/img/logo.png",
+},
+
+  
 ];
 
 const AudioContext = createContext();
