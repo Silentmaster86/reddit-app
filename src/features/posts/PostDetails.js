140a05b3b3988c3a0a4ff6180371ddfccc07fc34
Przemysław Pietkun <116354099+Silentmaster86@users.noreply.github.com>
2025-05-13 21:08:47 +0100

    Update PostDetails.js

--- src/features/posts/PostDetails.js
+++ src/features/posts/PostDetails.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import styled from "styled-components";
 import Spinner from "../../components/UI/Spinner.js";
+import CommentSection from "./CommentSection.js";
 
 const Wrapper = styled.div`
   padding: 2rem;
